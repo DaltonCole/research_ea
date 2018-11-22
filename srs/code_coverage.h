c4e55db99dc5474651f4390fa706b31e0cc8d23c
Dalton Cole <dalton_cl@yahoo.com>
2018-11-22 12:41:31 -0600

    initial commit

---
+++ srs/code_coverage.h
@@ -0,0 +1,7 @@
+class Code_coverage {
+	// Accept word, output percent of code covered
+	// 		might just overload the function operator
+	float code_coverage(string input) const;
+
+	string program_to_run;
+}

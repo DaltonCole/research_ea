9831374b4e6a085b10019f0cb300c0d0d71af202
Dalton Cole <dalton_cl@yahoo.com>
2018-11-28 14:19:10 -0600

    added gitignore

--- srs/tmp/0/index.js
+++
@@ -1,5 +0,0 @@
-var data = {files:[
-{"link":"mipl_parser.8159829a7e16cfa9/index.html","title":"mipl_parser","summary_name":"mipl_parser","covered_class":"linePartCov","covered":"66.8","covered_lines":"1222","uncovered_lines":"608","total_lines" : "1830"},
-], merged_files:[]};
-var percent_low = 25;var percent_high = 75;
-var header = { "command" : "mipl_parser", "date" : "2018-11-28 14:11:04", "instrumented" : 1830, "covered" : 1222,};
